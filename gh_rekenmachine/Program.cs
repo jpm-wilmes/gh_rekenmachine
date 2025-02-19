b6589a689f39058a0d44d6a0dc1614788511a8ec
Jeroen Wilmes <58515763+jpm-wilmes@users.noreply.github.com>
2025-02-19 11:20:53 +0100

    Verduidelijking

--- gh_rekenmachine/Program.cs
+++ gh_rekenmachine/Program.cs
@@ -8,7 +8,7 @@ V1.0:   Rechttoerechtaan (zie pdf in de map)
 V1.1:   Rekenen in een functie opgenomen
  */
 
-namespace RekenMachine
+namespace gh_rekenmachine
 {
     class RekenMachine
     {
@@ -22,7 +22,7 @@ namespace RekenMachine
         // 4: maak een exe
 
         // Dit is het hoofdprogramma
-        static void Main(string[] args)
+        static void Main()
         {
             // Voer het eerste getal in
             Console.WriteLine("Wat is het eerste getal:");
@@ -33,13 +33,16 @@ namespace RekenMachine
             // Voer de bewerking in
             Console.WriteLine("Wat is de bewerking:");
             string bewerking = Console.ReadLine();
+            // via de functie het resultaat opvragen. Komt meteen as string zodat het naar output kan
             string uitkomst = Berekenen(waarde1, waarde2, bewerking);
             Console.WriteLine("De uitkomst van " + waarde1 + " " + bewerking + " " + waarde2 + " = " + uitkomst);
          }
-
+        // Onderstaande functie voert het rekenwerk uit en zorgt dat de juiste types worden gebruikt
+        // werkt alleen met integers
+        // extra opdracht: breid uit met float en beveilig de input hierop (alleen berekenen als er daadwerkelijk een float is ingevoerd)
         private static string Berekenen(string w1, string w2, string bewerk)
         {
-            string resultaat = "";
+            string resultaat = null;
             switch (bewerk)
             {
                 case "+":
